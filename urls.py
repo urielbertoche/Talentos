7aedd45f4c35cf3c3d7823e179f76fe4a15d7536
Victor Fontes Costa <victor@t1000.local>
2010-08-18 12:49:04 -0300

    limpando o urls do projeto. fazendo com que o django só sirva media quando está em modo de debug

--- urls.py
+++ urls.py
@@ -7,16 +7,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^talentos/', include('talentos.foo.urls')),
+
     (r'^talentos/$', 'talentos.informacoes.views.exibir_formulario_conhecimento'),
     (r'^talentos/executar/$', 'talentos.informacoes.views.executar_formulario_conhecimento'),
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+
     (r'login/', 'django.contrib.auth.views.login')
 )
+
+
+# MEDIA 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
